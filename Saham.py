6a2f34beaa929e515a7d411bfbd5ab2a8902cf58
Arfan Hidayat Priyantono <arfan.hidayat.priyantono@gmail.com>
2024-12-19 22:24:38 +0700

    Update Saham.py

--- Saham.py
+++ Saham.py
@@ -13,14 +13,11 @@ def apply_global_css():
     button_style = """
     <style>
         div.stButton > button {
-            background-color: #0d6efd;
+            background-color: #2B8CCE;
             color: white;
-            border-radius: 8px;
             width: 100%;
             height: 50px;
             font-size: 18px;
-            box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.2);
-            transition: all 0.3s ease;
         }
         
     </style>
